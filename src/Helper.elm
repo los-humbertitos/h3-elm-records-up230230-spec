39dd2e79c868099bf75036951340d32ce525470c
Vane <up230230@alumnos.upa.edu.mx>
2026-03-01 23:52:52 -0600

    Tarea h5

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,98 +1,64 @@
-module Helper exposing (..)
+module Helper exposing
+    ( AirplaneStatus(..)
+    , Grade(..)
+    , airplaneScheduleAction
+    , airportAction
+    , categoricalGrade
+    )
 
-import Html exposing (Html, div, h1, li, text, ul)
-
-
-type alias Laptop =
-    { ram : String
-    , model : String
-    , brand : String
-    , screenSize : String
-    }
+-- TIPOS
 
 
-type alias Videogame =
-    { title : String
-    , releaseYear : Int
-    , available : Bool
-    , downloads : Int
-    , genres : List String
-    }
+type Grade
+    = Approved
+    | Failed
+    | Pending
 
 
-
--- Int
-
-
-add2 : Int -> Int -> Int
-add2 a b =
-    a + b
+type AirplaneStatus
+    = Cancelled
+    | Delayed
+    | OnTime
+    | Boarding
 
 
 
--- Float
+-- FUNCIONES
 
 
-add3 : Float -> Float -> Float -> Float
-add3 x y z =
-    x + y + z
+categoricalGrade : List Float -> List Grade
+categoricalGrade grades =
+    List.map gradeCategory grades
 
 
+gradeCategory : Float -> Grade
+gradeCategory g =
+    if g >= 7 then
+        Approved
 
--- Dos numeros y una funcion (op)
+    else if g >= 0 then
+        Failed
 
-
-calc : a -> a -> (a -> a -> b) -> b
-calc x y operation =
-    operation x y
-
-
-
--- Fun (Listas y Records)
-
-
-languageNames : List { record | name : String } -> List String
-languageNames list =
-    List.map .name list
+    else
+        Pending
 
 
-onlyStudents : List { record | name : String, uType : String } -> List String
-onlyStudents list =
-    List.map
-        (\u ->
-            if u.uType == "Student" then
-                u.name
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
 
-            else
-                ""
-        )
-        list
+        Delayed ->
+            "Esperar"
 
+        OnTime ->
+            "Esperar"
 
-getVideogameGenres : List Videogame -> List (List String)
-getVideogameGenres list =
-    List.map .genres list
+        Boarding ->
+            "Buscar boleto"
 
 
-myLaptop : Laptop
-myLaptop =
-    { ram = "16GB"
-    , model = "XPS 13"
-    , brand = "Dell"
-    , screenSize = "13.4"
-    }
-
-
-main : Html msg
-main =
-    div []
-        [ h1 [] [ text "My laptop" ]
-        , div []
-            [ ul []
-                [ li [] [ text ("Ram: " ++ myLaptop.ram) ]
-                , li [] [ text ("Modelo: " ++ myLaptop.model) ]
-                , li [] [ text ("Marca: " ++ myLaptop.brand) ]
-                , li [] [ text ("Pulgadas: " ++ myLaptop.screenSize) ]
-                ]
-            ]
-        ]
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
